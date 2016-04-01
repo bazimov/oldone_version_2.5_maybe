b638c31cadde8352f2f11efd7b1f316951b41972
Bekzot Azimov <bazimov@gogoair.com>
2016-03-31 22:11:48 -0500

    notifications are now working

--- app/src/main/java/com/ilmnuri/com/MyGcmListenerService.java
+++ app/src/main/java/com/ilmnuri/com/MyGcmListenerService.java
@@ -4,11 +4,13 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.graphics.Bitmap;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
@@ -64,9 +66,12 @@ public class MyGcmListenerService extends GcmListenerService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.logo);
+
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.rsz_logo)
+                .setSmallIcon(R.drawable.ic_notifications_none_24dp)
+                .setLargeIcon(image)
                 .setContentTitle("ilmnuri")
                 .setContentText(message)
                 .setAutoCancel(true)
